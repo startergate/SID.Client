9283d6e40c659f9e9ec14b88ee236423b2cc5195
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-04-04 20:21:08 +0900

    PHP 로그인 고침

--- SID.php/sid.php
+++ SID.php/sid.php
@@ -8,44 +8,45 @@
       private $clientName;
 
       // basic curl
-      private function curlPost($url, $data) {
-        $ch = curl_init();
+      private function curlPost($url, $data)
+      {
+          $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+          curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLOPT_URL, $url);
 
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-        if (!is_null($header)) {
-            curl_setopt($ch, CURLOPT_HEADER, true);
-        }
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-        $response = curl_exec($ch);
+          if (!is_null($header)) {
+              curl_setopt($ch, CURLOPT_HEADER, true);
+          }
+          curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+          curl_setopt($ch, CURLOPT_VERBOSE, true);
+          $response = curl_exec($ch);
 
-        $body = null;
-        // error
-        if (!$response) {
-            $body = curl_error($ch);
-            // HostNotFound, No route to Host, etc  Network related error
-            $http_status = -1;
-        } else {
-            //parsing http status code
-            $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            if (!is_null($header)) {
-                $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-                $header = substr($response, 0, $header_size);
-                $body = substr($response, $header_size);
-            } else {
-                $body = $response;
-            }
-        }
-        curl_close($ch);
-        return $body;
+          $body = null;
+          // error
+          if (!$response) {
+              $body = curl_error($ch);
+              // HostNotFound, No route to Host, etc  Network related error
+              $http_status = -1;
+          } else {
+              //parsing http status code
+              $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+              if (!is_null($header)) {
+                  $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+                  $header = substr($response, 0, $header_size);
+                  $body = substr($response, $header_size);
+              } else {
+                  $body = $response;
+              }
+          }
+          curl_close($ch);
+          return $body;
       }
 
       // Construct and Destruct Function
@@ -65,32 +66,30 @@
       }
 
       // Login functions
-      public function login($id, $pw)
+      public function login($clientid, $id, $pw)
       {
           try {
-              $conn = new mysqli('sid.donote.co', 'root', 'Wb4H9nn542', 'sid_userdata');
-              $id = $conn->real_escape_string($id);
-              $pw = hash('sha256', $pw);
-              $sql = "SELECT pw,nickname,pid FROM userdata WHERE id LIKE '$id'";	//user data select
-              $result = $conn->query($sql);
-              $row = $result->fetch_assoc();
-              $sqlpid = $row['pid'];
-              if ($pw === $row['pw']) {
-                  $output = 1;
-                  $_SESSION['pid'] = $row['pid'];
-                  $_SESSION['nickname'] = strip_tags($row['nickname']);
-                  if (!($this->checkExist($this->clientName.'_additional', 'pid', $row['pid']))) {
-                      $sql = 'INSERT INTO '.$this->clientName."_additional (pid) VALUES ('".$row['pid']."')";
-                      $conn->query($sql);
-                      $output++;
-                  }
-
-                  return $output;
-              } else {
-                  $output = 0;
+              $userdata = curlPost('http://localhost:3000/api/login', json_encode(array(
+                  "type" => "login",
+                  "clientid" => $clientid,
+                  "userid" => $id,
+                  "password" => $pw,
+                  "isAuth" => false,
+                  "isAuthOn" => false,
+                  "isWeb" => true,
+              )));
+              $userdata = json_decode($userdata)
+              if ($userdata['type'] === 'error') {
+                  return 0;
               }
+              $sqlpid = $userdata['pid'];
+              $output = 1;
+              $_SESSION['sid_sessid'] = $userdata['requested_data'][0];
+              $_SESSION['sid_pid'] = $userdata['requested_data'][1];
+              $_SESSION['sid_nickname'] = strip_tags($userdata['requested_data'][2]);
 
-              return $output;
+              return 1;
+
           } catch (\Exception $e) {
               return -1;
           }
