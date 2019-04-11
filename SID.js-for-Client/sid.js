09afe99f4d89074b84f6502e956d9e53a39b9e3a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-04-11 23:13:54 +0900

    Add constructor

--- SID.js-for-Client/sid.js
+++ SID.js-for-Client/sid.js
@@ -1,16 +1,22 @@
 /*jshint esversion: 9 */
 
 class SID {
-  constructor() {
+  constructor(clientname) {
+    if (typeof jQuery == 'undefined') {
+      throw new Error("SID.js for Client requires jQuery");
+    }
+    if (navigator.userAgent.indexOf('MSIE') !== -1 || info.indexOf('Trident') !== -1) { //Client Browser Detection
+      throw new Error("SID.js for Client don't support IE");
+    }
+    this.clientname = clientname;
+  }
+  getProfile(sessid) {
 
   }
-  getProfile() {
+  createClientID(devicedata) {
 
   }
-  createClientID() {
-
-  }
-  indexedDBCreater() {
+  indexedDBCreater(data) {
 
   }
 }
