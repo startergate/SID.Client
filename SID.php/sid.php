18c3d8a538520ecb87da3b2e9453f48e995aaa05
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-04-17 23:50:49 +0900

    Update curlPost()

--- SID.php/sid.php
+++ SID.php/sid.php
@@ -20,9 +20,6 @@
           curl_setopt($ch, CURLOPT_POST, true);
           curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-          if (!is_null($header)) {
-              curl_setopt($ch, CURLOPT_HEADER, true);
-          }
           curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
           curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
           curl_setopt($ch, CURLOPT_VERBOSE, true);
@@ -37,13 +34,7 @@
           } else {
               //parsing http status code
               $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-              if (!is_null($header)) {
-                  $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-                  $header = substr($response, 0, $header_size);
-                  $body = substr($response, $header_size);
-              } else {
-                  $body = $response;
-              }
+              $body = $response;
           }
           curl_close($ch);
           return $body;
