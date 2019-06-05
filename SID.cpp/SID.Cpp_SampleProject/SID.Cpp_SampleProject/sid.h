a4b65513da32ceb8afe269d9d83485a0c5b0ce13
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-06 01:54:15 +0900

    Change Class Name

--- SID.cpp/SID.Cpp_SampleProject/SID.Cpp_SampleProject/sid.h
+++ SID.cpp/SID.Cpp_SampleProject/SID.Cpp_SampleProject/sid.h
@@ -22,66 +22,75 @@ namespace
 	}
 }
 
-class SID {
+class SIDCpp {
 private:
 	std::string clientName;
 
-	Json::Value curlPost(std::string url, std::string data, std::string method = "POST") {
-		CURL *curl = NULL;
-		CURLcode res;
-
-		int httpCode(0);
-		std::unique_ptr<std::string> httpData(new std::string());
-
-		curl_global_init(CURL_GLOBAL_ALL);
-
-		curl = curl_easy_init();
-		if (curl) {
-			struct curl_slist *chunk = NULL;
-			curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
-			curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
-			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
-			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
-
-			chunk = curl_slist_append(chunk, "Accept: application/json");
-			chunk = curl_slist_append(chunk, "Content-Type: application/json");
-			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-			curl_easy_setopt(curl, CURLOPT_VERBOSE, true);
-
-			res = curl_easy_perform(curl);
-			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-			curl_easy_cleanup(curl);
-			if (httpCode == 200)
-			{
-				Json::Value data;
-				Json::Reader reader;
-				reader.parse(httpData.get()->c_str(), data);
-
-				return data;
-			}
-		}
-		Json::Value error;
-		error["error"] = 1;
-		return error;
-	}
+	Json::Value curlPost(std::string url, std::string data, std::string method = "POST");
 
 public:
-	SID(std::string clientName) {
-		this->clientName = clientName;
-	}
+	SIDCpp(std::string clientName);
 
-	~SID() {}
+	~SIDCpp();
 
-	int login(std::string clientid, std::string id, std::string pw) {
-		Json::Value senddata;
-		Json::FastWriter writer;
-		senddata["type"] = "login";
-		senddata["clientid"] = clientid;
-		senddata["userid"] = id;
-		senddata["password"] = pw;
-		Json::Value userdata = this->curlPost("http://sid.donote.co:3000/api/session", writer.write(userdata));
-	}
+	int login(std::string clientid, std::string id, std::string pw);
 };
+
+Json::Value SIDCpp::curlPost(std::string url, std::string data, std::string method) {
+	CURL *curl = NULL;
+	CURLcode res;
+
+	int httpCode(0);
+	std::unique_ptr<std::string> httpData(new std::string());
+
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	curl = curl_easy_init();
+	if (curl) {
+		struct curl_slist *chunk = NULL;
+		curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
+
+		chunk = curl_slist_append(chunk, "Accept: application/json");
+		chunk = curl_slist_append(chunk, "Content-Type: application/json");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, true);
+
+		res = curl_easy_perform(curl);
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+		curl_easy_cleanup(curl);
+		if (httpCode == 200)
+		{
+			Json::Value data;
+			Json::Reader reader;
+			reader.parse(httpData.get()->c_str(), data);
+
+			return data;
+		}
+	}
+	Json::Value error;
+	error["error"] = 1;
+	return error;
+}
+
+SIDCpp::SIDCpp(std::string clientName) {
+	this->clientName = clientName;
+}
+
+SIDCpp::~SIDCpp() {}
+
+int SIDCpp::login(std::string clientid, std::string id, std::string pw) {
+	Json::Value senddata;
+	Json::FastWriter writer;
+	senddata["type"] = "login";
+	senddata["clientid"] = clientid;
+	senddata["userid"] = id;
+	senddata["password"] = pw;
+	Json::Value userdata = this->curlPost("http://sid.donote.co:3000/api/session", writer.write(userdata));
+	return userdata;
+}
