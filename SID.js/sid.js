2cde1f5e7cb66e11b4f9f6d7d7089bee9455d46f
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-13 07:29:58 +0900

    Prepare SID.js

--- SID.js/sid.js
+++ SID.js/sid.js
@@ -1,1 +1,39 @@
-var request = require('request');
+/*jshint esversion: 9 */
+
+const request = require('request');
+
+class SID {
+  constructor() {
+
+  }
+
+  login() {
+
+  }
+
+  logout() {
+
+  }
+
+  register() {
+
+  }
+
+  getUserNickname() {
+
+  }
+
+  loginCheck() {
+
+  }
+
+  passwordCheck() {
+
+  }
+
+  getClientName() {
+
+  }
+}
+
+module.exports = new SID();
