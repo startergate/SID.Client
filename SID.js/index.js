0e8c9715f0d6ea958390fc62e67dc509d0ec08c7
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-08 19:49:47 +0900

    requests

--- SID.js/index.js
+++ SID.js/index.js
@@ -15,31 +15,58 @@ class SID {
   }
 
   login(clientid, id, pw) {
-    this.sidServerInstance.post('session', )
+    this.sidServerInstance.post('/session/', {
+      type: 'login',
+      clientid: clinetid,
+      userid: id,
+      password: pw,
+      isPermanent: false,
+      isWeb: true
+    }).then(response => {});
   }
 
   loginAuth(clientid, sessid) {
-
+    this.sidServerInstance.post('/session/', {
+      type: 'login',
+      clientid: clientid,
+      sessid: sessid
+    }).then(response => {});
   }
 
   logout(clientid, sessid) {
-
+    this.sidServerInstance.delete('/session/', {
+      type: 'logout',
+      clientid: clientid,
+      sessid: sessid
+    }).then(response => {});
   }
 
   register(clientid, id, pw, nickname = 'User') {
-
+    this.sidServerInstance.post('/user/', {
+      type: 'register',
+      clientid: clientid,
+      userid: userid,
+      nickname: nickname,
+      password: pw
+    }).then(response => {});
   }
 
   getUserNickname(clientid, sessid) {
+    this.sidServerInstance.post('/session/', {
 
+    }).then(response => {});
   }
 
   loginCheck(target) {
+    this.sidServerInstance.post('/session/', {
 
+    }).then(response => {});
   }
 
   passwordCheck(clientid, sessid, pw) {
+    this.sidServerInstance.post('/session/', {
 
+    }).then(response => {});
   }
 
   getClientName() {
